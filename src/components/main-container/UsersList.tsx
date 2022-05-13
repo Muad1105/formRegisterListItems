00bf823bd71b2aa5391a7d485b0e729967312366
muad1105 <muadnoushad@gmail.com>
2022-05-13 18:03:58 +0530

    3rd change

--- src/components/main-container/UsersList.tsx
+++ src/components/main-container/UsersList.tsx
@@ -5,14 +5,36 @@ import {} from '../../model';
 function UsersList() {
     const users = useSelector((state: AppStore) => state.register);
 
-    const value = JSON.stringify(users.userList);
-
+    const value = users.userList;
     return (
-        <div>
-            {/* {value.map((e, i) => {
-                return <div>{e.user}</div>;
-            })} */}
-            {value}
+        <div className="user-list">
+            <table>
+                <thead>
+                    <tr>
+                        <td>user</td>
+                        <td>email</td>
+                        <td>phone</td>
+                        <td>gender</td>
+                        <td>role</td>
+                        <td>dob</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {value.map((e, i) => {
+                        return (
+                            <tr key={i}>
+                                <td>{i + 1}</td>
+                                <td>{e.user}</td>
+                                <td>{e.email}</td>
+                                <td>{e.phone}</td>
+                                <td>{e.genderValue}</td>
+                                <td>{e.role}</td>
+                                <td>{e.dob}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
         </div>
     );
 }
